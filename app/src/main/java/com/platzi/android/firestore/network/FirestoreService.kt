11ccc093917ed536a27659ef9aff08a44b1265cb
Adan Galicia <ansg75@gmail.com>
2021-08-23 08:41:28 -0500

    Added Callback.kt, Crypto.kt, FirestoreService.kt and User.kt

---
+++ app/src/main/java/com/platzi/android/firestore/network/FirestoreService.kt
@@ -0,0 +1,38 @@
+package com.platzi.android.firestore.network
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.platzi.android.firestore.model.Crypto
+import com.platzi.android.firestore.model.User
+
+const val CRYPTO_COLLECTION_NAME = "crytos"
+const val USER_COLLECTION_NAME = "users"
+
+class FirestoreService(val firebaseFirestore : FirebaseFirestore) {
+
+    fun setDocument(data: Any, collectionName: String, id: String, callback: Callback<Void>){
+        firebaseFirestore.collection(collectionName).document(id).set(data)
+            .addOnSuccessListener { callback.onSuccess(null) }
+            .addOnFailureListener { exception -> callback.onFailed(exception) }
+    }
+
+    fun updateUser(user: User, callback: Callback<User>?) {
+        firebaseFirestore.collection(USER_COLLECTION_NAME).document(user.username)
+            .update("cryptosList", user.cryptosList)
+            .addOnSuccessListener{
+                result ->
+                    if (callback != null){
+                        callback.onSuccess(user)
+                    }
+            }
+            .addOnFailureListener { exception ->
+                if (callback != null) {
+                    callback.onFailed(exception)
+                }
+            }
+    }
+
+    fun updateCrypto(crypto: Crypto){
+        firebaseFirestore.collection(CRYPTO_COLLECTION_NAME).document(crypto.getDocumentId())
+            .update("available", crypto.available)
+    }
+}
